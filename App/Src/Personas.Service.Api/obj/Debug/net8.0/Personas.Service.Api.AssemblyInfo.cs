0aa4fb42e472e06251a67ee2fa7c22cd806b40b9
Anibal <anibaliym@gmail.com>
2024-01-01 16:50:59 -0300

    Se Agregar las referencias y librerías necesarias

--- App/Src/Personas.Service.Api/obj/Debug/net8.0/Personas.Service.Api.AssemblyInfo.cs
+++ App/Src/Personas.Service.Api/obj/Debug/net8.0/Personas.Service.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Personas.Service.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dffcdfc1cc46d49d2d9dddf6f3293fc96f24fc13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Personas.Service.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Personas.Service.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
